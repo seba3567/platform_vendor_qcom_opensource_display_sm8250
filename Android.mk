9798b4191cb6111078895f7bc651d362379d4e68
ix5 <ix5@users.noreply.github.com>
2020-05-01 21:27:54 +0200

    treewide: Cleanup includes, include logic
    Android.bp: `subdirs` is deprecated and unnceeded in Q.
    
    Top-level Android.mk: Organize for better readability,
    account for platforms named `sdm` in addition to `msm`.
    
    Treewide: Remove empty `common_includes`.

--- Android.mk
+++ Android.mk
@@ -1,19 +1,27 @@
-sdm-libs := sdm/libs
-display-hals := include $(sdm-libs)/utils $(sdm-libs)/core libdebug gpu_tonemapper
+ifneq ($(TARGET_DISABLE_DISPLAY),true)
+
+display-hals := \
+    include\
+    sdm/libs/utils\
+    sdm/libs/core\
+    libdebug
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
-                    libdrmutils libhistogram drm.vendor
+display-hals += \
+    libcopybit \
+    liblight \
+    libmemtrack \
+    hdmi_cec \
+    libhistogram \
+    drm.vendor
+    gpu_tonemapper \
+    libdrmutils
 endif
 
 display-hals += gralloc
 display-hals += sde-drm
 display-hals += composer
 
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-else
-ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
-endif
+include $(call all-named-subdir-makefiles,$(display-hals))
+
+endif #TARGET_DISABLE_DISPLAY
